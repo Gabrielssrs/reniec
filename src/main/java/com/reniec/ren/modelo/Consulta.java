b98332a29cb9bef8470cc5bca93798e374901716
Gabriel Ramirez <ramirezsolongabriel@gmail.com>
2025-07-18 00:56:08 -0500

    cita2

--- src/main/java/com/reniec/ren/modelo/Consulta.java
+++ src/main/java/com/reniec/ren/modelo/Consulta.java
@@ -21,4 +21,6 @@ public class Consulta {
     @ManyToOne
     @JoinColumn(name = "id_cita")
     private Cita cita;
+
+    
 }
--- src/main/java/com/reniec/ren/repository/CitaRepository.java
+++ src/main/java/com/reniec/ren/repository/CitaRepository.java
@@ -5,6 +5,8 @@ package com.reniec.ren.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.reniec.ren.modelo.Cita;
+import com.reniec.ren.modelo.Consulta;
 
 public interface CitaRepository extends JpaRepository<Cita, Integer> {
+	Consulta findByDniAndCita_IdCita(String dni, Integer id_cita);
 }
