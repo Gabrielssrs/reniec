594ba9130ea0a5a3bc65d6ee0e8d71c360aa863e
Gabriel Ramirez <ramirezsolongabriel@gmail.com>
2025-07-18 01:04:29 -0500

    consutla2

--- src/main/java/com/reniec/ren/repository/CitaRepository.java
+++ src/main/java/com/reniec/ren/repository/CitaRepository.java
@@ -5,8 +5,7 @@ package com.reniec.ren.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.reniec.ren.modelo.Cita;
-import com.reniec.ren.modelo.Consulta;
 
 public interface CitaRepository extends JpaRepository<Cita, Integer> {
-	Consulta findByDniAndCita_IdCita(String dni, Integer id_cita);
+    // Métodos personalizados aquí si necesitas buscar por campos de Cita
 }
--- src/main/java/com/reniec/ren/repository/ConsultaRepository.java
+++ src/main/java/com/reniec/ren/repository/ConsultaRepository.java
@@ -6,5 +6,7 @@ import com.reniec.ren.modelo.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
-    Consulta findByDniAndIdCita(String dni, Long id_cita);
+    Consulta findByDniAndIdCita(String dni, Long id_cita); // si tienes el campo id_cita en Consulta
+    // O si tienes la relación:
+    // Consulta findByDniAndCita_IdCita(String dni, Integer id_cita);
 }
